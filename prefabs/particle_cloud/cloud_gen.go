80d420ad63585f72f24f633dad6f60670cb4095f
SAYURI\AdminSayuri <elgunalp1@gmail.com>
2022-03-02 15:52:23 +0300

    Started particle cloud generator.

---
+++ prefabs/particle_cloud/cloud_gen.go
@@ -0,0 +1,33 @@
+components {
+  id: "script"
+  component: "/prefabs/particle_cloud/cloud_gen.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/prefabs/particle_cloud/cloud_particle.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
